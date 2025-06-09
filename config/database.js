4f610242ae153e4bcb93256630fb161518b96f6d
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-06-10 00:23:08 +0700

    update: db to production

--- config/database.js
+++ config/database.js
@@ -18,4 +18,18 @@ module.exports = {
       },
     },
   },
+  production: {
+    username: config.user,
+    password: config.password,
+    database: config.database,
+    host: config.host,
+    port: config.port,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
